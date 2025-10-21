a3780160d6ac1e0188203e4bb218909e76378252
onePlaceholder <guekbaile@gmail.com>
2025-10-21 09:29:05 +0200

    updated the hashes

--- build/build.sh
+++ build/build.sh
@@ -4,6 +4,7 @@ pack_file=$(pwd)/../pack.toml
 commit_hash=$(git rev-parse --short=6 HEAD)
 version=$(grep -E '^version\s*=' $pack_file | sed -E 's/version\s*=\s*"([^"]+)"/\1/')
 
-output_name=another_edition-${version}-${commit_hash}.mrpack
+output_name=another_edition-${version}-${commit_hash}
 
-packwiz mr export --pack-file $pack_file --output $output_name
+packwiz mr export --pack-file $pack_file --output ${output_name}.mrpack
+packwiz cf export --pack-file $pack_file --output ${output_name}.zip
